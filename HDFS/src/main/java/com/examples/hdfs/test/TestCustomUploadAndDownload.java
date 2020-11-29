3fa8126ea3d17f9af1960807235c75e308cfdf77
root <771713194@qq.com>
2020-11-29 23:03:47 +0800

    添加自定义下传

--- HDFS/src/main/java/com/examples/hdfs/test/TestCustomUploadAndDownload.java
+++ HDFS/src/main/java/com/examples/hdfs/test/TestCustomUploadAndDownload.java
@@ -54,4 +54,70 @@ public class TestCustomUploadAndDownload {
 
         fs.close();
     }
+
+    @Test
+    public void TestFirstBlock() throws IOException {   //下载第一块
+        Path src = new Path("/深度学习和数据分析1.docx");
+        Path dst = new Path("C:\\Users\\james\\Desktop\\深度学习和数据分析first");
+        //本地文件创建一个输入流
+        FSDataInputStream is = fs.open(src);
+        //hdfs文件系统创建一个输出流
+        FSDataOutputStream os = localFs.create(dst, true);
+
+        //流的拷贝
+        byte[] buffer = new byte[1024];   //1K大小数据
+        for (int i=0; i < 100; i++){
+            is.read(buffer);
+            os.write(buffer);
+        }
+
+        //关流
+        IOUtils.closeStream(is);
+        IOUtils.closeStream(os);
+
+        fs.close();
+    }
+
+    @Test
+    public void TestSecondBlock() throws IOException {  //下载第二块
+        Path src = new Path("/深度学习和数据分析1.docx");
+        Path dst = new Path("C:\\Users\\james\\Desktop\\深度学习和数据分析second");
+        //本地文件创建一个输入流
+        FSDataInputStream is = fs.open(src);
+        //hdfs文件系统创建一个输出流
+        FSDataOutputStream os = localFs.create(dst, true);
+
+        //定位位置
+        is.seek(1024 * 100);
+        //流的拷贝
+        byte[] buffer = new byte[1024];   //1K大小数据
+        for (int i=0; i < 100; i++){
+            is.read(buffer);
+            os.write(buffer);
+        }
+
+        //关流
+        IOUtils.closeStream(is);
+        IOUtils.closeStream(os);
+
+        fs.close();
+    }
+
+    @Test
+    public void TestFinallyBlock() throws IOException {  //下载最后一块
+        Path src = new Path("/深度学习和数据分析1.docx");
+        Path dst = new Path("C:\\Users\\james\\Desktop\\深度学习和数据分析second");
+        //本地文件创建一个输入流
+        FSDataInputStream is = fs.open(src);
+        //hdfs文件系统创建一个输出流
+        FSDataOutputStream os = localFs.create(dst, true);
+
+        //定位位置
+        is.seek(1024 * 100 * 2);
+        //流的拷贝,buffSize不能超过4096
+        IOUtils.copyBytes(is, os, 4096, true);
+
+        fs.close();
+    }
+
 }
